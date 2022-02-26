4e470bf5fe76a55887735fa0274f0761d2325d2d
JacobRose <95928931+JacobRose43@users.noreply.github.com>
2022-02-26 22:08:47 +0100

    update

--- expressData/firstApp/script.js
+++ expressData/firstApp/script.js
@@ -1,10 +1,36 @@
 const express = require("express");
 const app = express();
+const port = 8080;
 
-app.use(() => {
-	console.log("WE GOT A NEW REQUEST");
+// app.use((req, res) => {
+// 	console.log("WE GOT A NEW REQUEST");
+// 	res.send("Welcome!");
+// });
+
+app.get("/", (req, res) => {
+	res.send("HOME PAGE!");
 });
 
-app.listen(3000, () => {
-	console.log("LISTENING ON PORT 3000");
+app.get("/r/:query", (req, res) => {
+	console.log(req.params);
+	res.send("Here is your search!");
 });
+
+app.get("/contact", (req, res) => {
+	res.send("jacobrose.433@gmail.com");
+});
+
+app.listen(port, () => {
+	console.log(`LISTENING ON PORT ${port}`);
+});
+
+app.get("/search", (req, res) => {
+	const { q } = req.query;
+	res.send(`Search result for: ${q}`);
+});
+
+app.get("*", (req, res) => {
+	res.send("I dont know that path!");
+});
+
+// that have to be on the end, because path "*" belongs to everything
