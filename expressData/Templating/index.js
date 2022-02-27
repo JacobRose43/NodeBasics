26910c827577ef8d48c4c2b9d9a9ddef429bfd84
JacobRose <95928931+JacobRose43@users.noreply.github.com>
2022-02-27 13:47:57 +0100

    update

--- expressData/Templating/index.js
+++ expressData/Templating/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const path = require("path");
 
-app.set("view engine", "ejs");
-// ^^^ express behind the scene, require automatic ejs
+app.set("view engine", "ejs"); // <<< express behind the scene, require automatic ejs
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
 	res.render("home.ejs"); // default /views/<file>
