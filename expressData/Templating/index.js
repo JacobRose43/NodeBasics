d1ffd8588cdbb969649a62117fe7ea1070413542
JacobRose <95928931+JacobRose43@users.noreply.github.com>
2022-02-28 15:10:37 +0100

    update

--- expressData/Templating/index.js
+++ expressData/Templating/index.js
@@ -2,12 +2,19 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const redditData = require("./data.json");
 
 app.set("view engine", "ejs"); // <<< express behind the scene, require automatic ejs
 app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
 	res.render("home.ejs"); // default /views/<file>
+});
+
+app.get("/r/:subreddit", (req, res) => {
+	const { subreddit } = req.params;
+	const data = redditData[subreddit];
+	res.render("subreddit", { ...data });
 });
 
 app.listen(port, () => {
