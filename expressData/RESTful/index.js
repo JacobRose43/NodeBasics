0dd8c6765bd565b5570f794b9041a5c2db313edf
JacobRose <95928931+JacobRose43@users.noreply.github.com>
2022-03-05 22:45:58 +0100

    update

--- expressData/RESTful/index.js
+++ expressData/RESTful/index.js
@@ -2,17 +2,20 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const port = 8080;
-const { v4: uId } = require("uuid");
+const {
+	v4: uId
+} = require("uuid");
 const methodOverride = require("method-override");
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+	extended: true
+}));
 app.use(express.json());
 app.use(methodOverride("_method"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-const comments = [
-	{
+let comments = [{
 		id: uId(),
 		username: "Todd",
 		comment: "Lol first comment!",
@@ -35,7 +38,9 @@ const comments = [
 ];
 
 app.get("/comments", (req, res) => {
-	res.render("comments/index", { comments });
+	res.render("comments/index", {
+		comments
+	});
 });
 
 app.get("/comments/new", (req, res) => {
@@ -43,37 +48,65 @@ app.get("/comments/new", (req, res) => {
 });
 
 app.post("/comments", (req, res) => {
-	const { username, comment } = req.body;
-	comments.push({ username, comment, id: uId() });
+	const {
+		username,
+		comment
+	} = req.body;
+	comments.push({
+		username,
+		comment,
+		id: uId()
+	});
 	res.redirect("/comments");
 });
 
 app.get("/comments/:id", (req, res) => {
-	const { id } = req.params;
+	const {
+		id
+	} = req.params;
 	const comment = comments.find((c) => c.id === id);
-	res.render("comments/show", { comment });
+	res.render("comments/show", {
+		comment
+	});
 });
 
 app.get("/comments/:id/edit", (req, res) => {
-	const { id } = req.params;
+	const {
+		id
+	} = req.params;
 	const comment = comments.find((c) => c.id === id);
-	res.render("comments/edit", { comment });
+	res.render("comments/edit", {
+		comment
+	});
 });
 
 app.patch("/comments/:id", (req, res) => {
-	const { id } = req.params;
+	const {
+		id
+	} = req.params;
 	const newCommentText = req.body.comment;
 	const foundComment = comments.find((c) => c.id === id);
 	foundComment.comment = newCommentText;
 	res.redirect("/comments");
 });
 
+app.delete('/comments/:id', (req, res) => {
+	const {
+		id
+	} = req.params;
+	comments = comments.filter(c => c.id !== id);
+	res.redirect("/comments");
+})
+
 app.get("/dinner", (req, res) => {
 	res.send("GET/dinner resposne");
 });
 
 app.post("/dinner", (req, res) => {
-	const { meat, qty } = req.body;
+	const {
+		meat,
+		qty
+	} = req.body;
 	res.send(`Your ${qty} ${meat}`);
 });
 
@@ -81,4 +114,3 @@ app.post("/dinner", (req, res) => {
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
 });
-
